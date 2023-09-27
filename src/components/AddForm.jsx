b4e76421728096f492af1cb5f80715c27a32c835
Michal Kurek (Mike) <michal@kurek.pl>
2023-09-27 17:31:15 +0200

    first step of AddForm build

--- src/components/AddForm.jsx
+++ src/components/AddForm.jsx
@@ -1,9 +1,20 @@
+import { useState } from "react";
+
 export default function AddForm() {
+  const [value, setValue] = useState("");
+
+  const changeValue = e => {
+    setValue(e.target.value);
+    console.log(value);
+  };
+
   return (
     <form className="add-form">
       <input
+        value={value}
         type="text"
         className="add-form__input"
+        onChange={e => changeValue(e)}
       />
       <button className="add-form__btn">Add Task</button>
     </form>
